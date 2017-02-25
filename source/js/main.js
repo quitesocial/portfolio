89658cd325e903e077bd167afd7828efade2fe19
quitesocial <quitesocial@yandex.ru>
2017-02-25 19:10:32 +0300

    fixed parallax error bug

--- source/js/main.js
+++ source/js/main.js
@@ -25,31 +25,3 @@
         // console.log(initialX, initialY);
     });
 })();
-
-(function () {
-    var parallaxContainer = document.getElementById('parallaxLeaves'),
-        layers = parallaxContainer.getElementsByClassName('parallax-layer');
-
-    window.addEventListener('mousemove', function(e) {
-        var pageX = e.pageX,
-            pageY = e.pageY,
-            initialX = (window.innerWidth / 2) - pageX,
-            initialY = (window.innerHeight / 2) - pageY;
-
-        [].slice.call(layers).forEach(function(layer, i) {
-            var divider = (i + 1) / 100,
-                positionX = initialX * divider,
-                positionY = initialY * divider,
-                layerStyle = layer.style,
-                transformString = 'translate3d(' + positionX + 'px, ' + positionY + 'px, 0)';
-
-            layerStyle.transform = transformString;
-            layerStyle.msTransform = transformString;
-            layerStyle.webkitTransform = transformString;
-            layerStyle.oTransform = transformString;
-            layerStyle.mozTransform = transformString;
-        });
-
-        // console.log(initialX, initialY);
-    });
-})();
