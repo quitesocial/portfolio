027e5bfa7a3d6e665d5361afd8275790981f1fce
quitesocial <quitesocial@yandex.ru>
2017-03-04 21:06:38 +0300

    blog nav done for desktop BP

--- source/js/main.js
+++ source/js/main.js
@@ -69,18 +69,6 @@ let flipper = (() => {
 
 // hamburger main menu
 
-// let menu = (() => {
-//     return {
-//         init: function init() {
-//             let hamburger = $('.menu-button'),
-//                 mainMenu = $('.main-menu');
-//             hamburger.on('click', () => {
-//                 mainMenu.toggleClass('main-menu_opened');
-//             });
-//         }
-//     }
-// })();
-
 let menu = (() => {
     return {
         init: function init() {
@@ -94,12 +82,76 @@ let menu = (() => {
     }
 })();
 
+// sticky blog navigation
+
+let blogMenu = (() => {
+    return {
+        init: function init() {
+            let nav = $('.blog-nav__list'),
+                startPos = nav.offset().top;
+            $(window).scroll(() => {
+                if ($(window).scrollTop() >= startPos) {
+                    if (nav.hasClass() == false) {
+                        nav.addClass('blog-nav__list_totop');
+                    }
+                }
+                else {
+                    nav.removeClass('blog-nav__list_totop');
+                }
+            });
+        }
+    }
+})();
+
+// active post blog navigation scroll
+
+let activePostChanger = (() => {
+    return {
+        init: function init() {
+            var menu_selector = ".blog-nav";
+
+            function onScroll() {
+                var scroll_top = $(document).scrollTop() / 1.08;
+                $(menu_selector + " a").each(function () {
+                    var hash = $(this).attr("href");
+                    var target = $(hash);
+                    if (target.position().top <= scroll_top && (target.position().top * 2) + target.outerHeight() > scroll_top) {
+                        $(menu_selector + " a.blog-nav__link_active").removeClass("blog-nav__link_active");
+                        $(this).addClass("blog-nav__link_active");
+                    } else {
+                        $(this).removeClass("blog-nav__link_active");
+                    }
+                });
+            };
+            $(document).on("scroll", onScroll);
+            $("a[href^='#']").click(function (e) {
+                e.preventDefault();
+                $(document).off("scroll");
+                $(menu_selector + " a.blog-nav__link_active").removeClass("blog-nav__link_active");
+                $(this).addClass("blog-nav__link_active");
+                var hash = $(this).attr("href");
+                var target = $(hash);
+                $("html, body").animate({
+                    scrollTop: target.offset().top
+                }, 500, function () {
+                    window.location.hash = hash;
+                    $(document).on("scroll", onScroll);
+                });
+            });
+        }
+    }
+})();
+
 // init block
 
 window.onload = () => {
     parallaxMouseMove.init();
     flipper.init();
     menu.init();
+    if ($('.blog-nav__list').length) {
+        blogMenu.init();
+        activePostChanger.init();
+    }
 };
 
 window.onscroll = () => {
